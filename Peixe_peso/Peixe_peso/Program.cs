51c2300f253c6640b9e0b1f3ad89f78fb5a2afaf
AndreiVDestro <andreidestro535@gmail.com>
2025-03-26 11:21:16 -0300

    Multa do peixe

---
+++ Peixe_peso/Peixe_peso/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peixe_peso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double peso;
+            double sobra;
+            Console.Write("digite o peso do pescado: ");
+            peso=int.Parse(Console.ReadLine());
+            sobra = (peso - 50) * 4;
+            Console.Write(sobra);
+        }
+    }
+}
