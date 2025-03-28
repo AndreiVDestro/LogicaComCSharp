d6656ab3a1f78eb03802c12cb57342e07e6de514
AndreiVDestro <andreidestro535@gmail.com>
2025-03-28 08:38:02 -0300

    Bomba

---
+++ Bomba Enquanto/Bomba Enquanto/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bomba_Enquanto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 10;
+            while (contador >0) 
+            {
+                Console.WriteLine("Detonaçao "+ contador+ "\n");
+                contador = contador - 1;
+            }
+            Console.WriteLine("Boooommm");
+        }
+    }
+}
