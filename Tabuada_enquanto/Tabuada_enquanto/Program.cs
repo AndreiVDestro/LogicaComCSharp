2e677bc4d7261a150e7a597dc010583e9bbaee18
AndreiVDestro <andreidestro535@gmail.com>
2025-03-28 09:28:41 -0300

    Tabuada_enquanto
    Tabuada usando While

---
+++ Tabuada_enquanto/Tabuada_enquanto/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabuada_enquanto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, resultado, contador = 1;
+            Console.WriteLine("Escreva um numero");
+            numero=int.Parse(Console.ReadLine());
+            while (contador < 10)
+            {
+                contador = contador + 1;
+                resultado = numero * contador;
+                Console.WriteLine(numero + "X" + contador + "=" + resultado + "\n");
+            }
+
+                
+
+        }
+    }
+}
