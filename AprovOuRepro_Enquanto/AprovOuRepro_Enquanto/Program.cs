84e028887216fa167471f271e78ae48f14041488
AndreiVDestro <andreidestro535@gmail.com>
2025-03-28 11:21:20 -0300

    Aprovado_enquanto

---
+++ AprovOuRepro_Enquanto/AprovOuRepro_Enquanto/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AprovOuRepro_Enquanto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 1, reprovado, aprovado, estado;
+            aprovado = 0;
+            reprovado = 0;
+            while(contador < 400)
+            {
+                Console.WriteLine("Peça: " + contador + "\n");
+                Console.WriteLine("digite(1)APROVADO e (2)REPROVADO: ");
+                estado=int.Parse(Console.ReadLine());
+                contador++;
+                if(estado == 1)
+                {
+                    aprovado++;
+                }
+                else if(estado== 2){
+                    reprovado++;
+                }
+                else
+                    {
+                        Console.WriteLine("Estado invalido digite numero 1 ou 2");
+                    }
+                Console.WriteLine("Aprovados: " + aprovado + "\nReprovados: " + reprovado);
+            }
+        }
+    }
+}
