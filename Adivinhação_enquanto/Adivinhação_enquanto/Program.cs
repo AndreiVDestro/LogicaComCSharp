9cc0b48e33eab0fa8e3ce2d79974d01224f76822
AndreiVDestro <andreidestro535@gmail.com>
2025-03-28 09:46:29 -0300

    Adivinhaçao_enquanto

---
+++ Adivinhação_enquanto/Adivinhação_enquanto/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adivinhação_enquanto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, acertos, contador, advin;
+            contador = 0;
+            acertos = 0;
+            Console.WriteLine("\n JOGADOR 1 Digite um numero de 1 a 10 \n");
+            numero=int.Parse(Console.ReadLine());
+            while (contador < 11) 
+            {
+                Console.WriteLine(" JOGADOR 2 Adivinhe o numero escolhido \n");
+                advin=int.Parse(Console.ReadLine());
+                contador=contador+1;
+                if (advin == numero)
+                {
+                    acertos = acertos + 1;
+                    Console.WriteLine(" VOCÊ ACERTOU O NUMERO " + advin + " NA TENTATIVA " + contador + "\n");
+
+                }
+            }
+        }
+    }
+}
