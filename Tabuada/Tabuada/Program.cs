0fb7a7a4ae62fb023584adca786cd0afc6da9777
AndreiVDestro <andreidestro535@gmail.com>
2025-03-28 08:57:14 -0300

    TabuadaFor
    Usando For

---
+++ Tabuada/Tabuada/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, resultado, contador;
+            Console.WriteLine("Escreva um numero: ");
+            numero=int.Parse(Console.ReadLine());
+            for (contador = 1; contador <= 10; contador++) 
+            {
+                resultado=numero*contador;
+                Console.WriteLine(numero+ "X"+ contador+ "="+ resultado);
+            }
+
+        }
+    }
+}
