ca5fe817b5bddbfaa1769167dfbeab266d05999c
AndreiVDestro <andreidestro535@gmail.com>
2025-03-28 08:25:17 -0300

    Bomba para
    Contagem usando o For

---
+++ Bomba_para1/Bomba_para1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bomba_para1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador;
+            for (contador = 0; contador <= 9; contador++) ;
+            {
+                Console.WriteLine(contador);
+            }
+            
+
+                
+        }
+    }
+}
---
+++ NumeroSequancia/NumeroSequancia/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumeroSequancia
+{
+    internal class Program
+    {
+        static void Main(string[] args,
+        {
+            int numero;
+            double m,b;
+            Console.Write("Escreva seu numero: ");
+            numero = Console.Read();
+            b = (numero - 1);
+            m = (numero + 1);
+            Console.Write(b, numero, m);
+        }
+    }
+}
